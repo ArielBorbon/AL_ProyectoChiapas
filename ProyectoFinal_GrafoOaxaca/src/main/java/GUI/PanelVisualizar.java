02a53165e2a4bddc8cad0838926fd7a1432ff201
Pedro Luna <106363641+iPeterMoon@users.noreply.github.com>
2025-05-15 09:34:39 -0700

    Accion Mostrar Grafo Visual

--- ProyectoFinal_GrafoOaxaca/src/main/java/GUI/PanelVisualizar.java
+++ ProyectoFinal_GrafoOaxaca/src/main/java/GUI/PanelVisualizar.java
@@ -57,7 +57,7 @@ public class PanelVisualizar extends JPanel {
 });
 
         btnGrafo.addActionListener(e -> {
-
+            mostrarGrafo();
         });
         btnVolver.addActionListener(e -> {
             // Obtener el JFrame (ventana) que contiene este panel
