12f249227851b2bd11d4d8874fc04dce7fe1469d
ArielBorbon <ariel.borbon252116@potros.itson.edu.mx>
2025-05-17 22:39:40 -0700

    T(n) panelGrafo

--- ProyectoFinal_GrafoOaxaca/src/main/java/GUI/PanelGrafo.java
+++ ProyectoFinal_GrafoOaxaca/src/main/java/GUI/PanelGrafo.java
@@ -16,7 +16,7 @@ import Implementacion.Vertice;
 import java.awt.Dimension;
 import java.util.Map;
 
-public class PanelGrafo {
+public class PanelGrafo {                           //                                  T(n) =  119n"2 + 94n + 95         O(n"2)
 
     private static final GrafoChiapas grafoChiapas = new GrafoChiapas(); // Instancia del grafo principal
     public static final String LA_HOJA_DE_ESTILOS = """
@@ -51,9 +51,9 @@ public class PanelGrafo {
      *
      * @return Un JPanel que contiene la visualización del grafo.
      */
-    public static JPanel obtenerPanelGrafo() {
-        Graph graph = crearGrafoChiapas();
-        return createPanelGrafo(graph);
+    public static JPanel obtenerPanelGrafo() {                                                  //                                  19n"2 + 18n + 18
+        Graph graph = crearGrafoChiapas();  // 19n"2    +   18n   + 10
+        return createPanelGrafo(graph);         //8
     }
 
     /* Metodo Antiguo
@@ -97,36 +97,36 @@ public class PanelGrafo {
         return createPanelGrafo(grafoChiapasVisual);
     }
      */
-    public static JPanel createPanelGrafo(Graph grafo) {
-        SwingViewer viewer = new SwingViewer(grafo, SwingViewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
-        viewer.disableAutoLayout();
-        ViewPanel viewPanel = (ViewPanel) viewer.addDefaultView(false);
-        return viewPanel;
+    public static JPanel createPanelGrafo(Graph grafo) {                                                                                                                //7
+        SwingViewer viewer = new SwingViewer(grafo, SwingViewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);      //3
+        viewer.disableAutoLayout();                                         //1
+        ViewPanel viewPanel = (ViewPanel) viewer.addDefaultView(false);         //2
+        return viewPanel;                  //1
     }
 
-    static Graph crearGrafoChiapas() {
-        System.setProperty("org.graphstream.ui", "swing");
-        Graph grafo = new SingleGraph("Grafo Chiapas");
-        int[][] coordenadas = grafoChiapas.getCoordenadas();
-        for (Vertice ciudad : grafoChiapas.getGrafo().obtenerVertices()) {
-            Node vertice = grafo.addNode(ciudad.getNombre());
-            vertice.setAttribute("ui.label", ciudad.getNombre());
-            int indice = grafoChiapas.getCiudades().indexOf(ciudad);
-            vertice.setAttribute("xy", coordenadas[indice][0], coordenadas[indice][1]);
+    static Graph crearGrafoChiapas() {          //                                                                                                                19n"2    +   18n   + 9
+        System.setProperty("org.graphstream.ui", "swing");                  //1
+        Graph grafo = new SingleGraph("Grafo Chiapas");                  //2
+        int[][] coordenadas = grafoChiapas.getCoordenadas();                  //2
+        for (Vertice ciudad : grafoChiapas.getGrafo().obtenerVertices()) {                  //3n +1
+            Node vertice = grafo.addNode(ciudad.getNombre());                  //3n
+            vertice.setAttribute("ui.label", ciudad.getNombre());                  //2n
+            int indice = grafoChiapas.getCiudades().indexOf(ciudad);                  //3n
+            vertice.setAttribute("xy", coordenadas[indice][0], coordenadas[indice][1]);                  //3n
         }
 
-        for (Vertice ciudad : grafoChiapas.getGrafo().obtenerVertices()) {
-            for (Arista arista : grafoChiapas.getGrafo().obtenerAdyacentes(ciudad)) {
-                String origen = arista.getOrigen().getNombre();
-                String destino = arista.getDestino().getNombre();
-                if (grafo.getEdge(origen + "-" + destino) == null && grafo.getEdge(destino + "-" + origen) == null) {
-                    Edge edge = grafo.addEdge(origen + "-" + destino, origen, destino);
+        for (Vertice ciudad : grafoChiapas.getGrafo().obtenerVertices()) {                  //3n+1
+            for (Arista arista : grafoChiapas.getGrafo().obtenerAdyacentes(ciudad)) {                  //(3n+1)*n
+                String origen = arista.getOrigen().getNombre();                  //3n*n
+                String destino = arista.getDestino().getNombre();                  //3n*n
+                if (grafo.getEdge(origen + "-" + destino) == null && grafo.getEdge(destino + "-" + origen) == null) {                  //6n*n
+                    Edge edge = grafo.addEdge(origen + "-" + destino, origen, destino);                  //4n*n
                     edge.setAttribute("ui.label", arista.getDistancia());
                 }
             }
         }
 
-        grafo.setAttribute("ui.stylesheet", """
+        grafo.setAttribute("ui.stylesheet", """                  
         node {
           fill-color: rgb(134,192,160); /* color base */
           size: 30px;
@@ -147,52 +147,52 @@ public class PanelGrafo {
           fill-color: red;
           size: 3px;
         }
-    """);
+    """);//1
 
-        return grafo;
+        return grafo;                  //1
     }
 
-    public static JPanel createPanelGrafo(Graph grafo, boolean autoLayout) {
+    public static JPanel createPanelGrafo(Graph grafo, boolean autoLayout) {                                                                               //13
 
         SwingViewer viewer = new SwingViewer(grafo,
-                SwingViewer.ThreadingModel.GRAPH_IN_GUI_THREAD
-        );
-        if (autoLayout) {
-            viewer.enableAutoLayout();
+                SwingViewer.ThreadingModel.GRAPH_IN_GUI_THREAD                  //3
+        );                                  
+        if (autoLayout) {                  //1
+            viewer.enableAutoLayout();                  //1
         } else {
-            viewer.disableAutoLayout();
+            viewer.disableAutoLayout();                  //1
         }
 
-        ViewPanel viewPanel = (ViewPanel) viewer.addDefaultView(false);
+        ViewPanel viewPanel = (ViewPanel) viewer.addDefaultView(false);                  //2
 
-        viewPanel.setPreferredSize(new Dimension(800, 600));
-        viewPanel.setMinimumSize(new Dimension(400, 300));
+        viewPanel.setPreferredSize(new Dimension(800, 600));                  //2
+        viewPanel.setMinimumSize(new Dimension(400, 300));                  //2
 
-        return viewPanel;
+        return viewPanel;                  //1
     }
 
-    public static JPanel obtenerGrafoPintado(GrafoTDA subGrafo) {
-        Graph g = crearGrafoChiapas();
+    public static JPanel obtenerGrafoPintado(GrafoTDA subGrafo) {                                                                               // 41n"2 + 27n + 25
+        Graph g = crearGrafoChiapas();      // 19n"2    +   18n   + 9   
 
-        for (Vertice u : subGrafo.obtenerVertices()) {
-            for (Arista a : subGrafo.obtenerAdyacentes(u)) {
-                String id1 = a.getOrigen().getNombre() + "-" + a.getDestino().getNombre();
-                Edge e = g.getEdge(id1);
-                if (e == null) {
-                    e = g.getEdge(a.getDestino().getNombre() + "-" + a.getOrigen().getNombre());
+        for (Vertice u : subGrafo.obtenerVertices()) {                                        //2n +1 
+            for (Arista a : subGrafo.obtenerAdyacentes(u)) {                                      //(2n +1 )*n
+                String id1 = a.getOrigen().getNombre() + "-" + a.getDestino().getNombre();                  //7n*n
+                Edge e = g.getEdge(id1);                //2n*n
+                if (e == null) {                                //n*n
+                    e = g.getEdge(a.getDestino().getNombre() + "-" + a.getOrigen().getNombre());                  //8n*n
                 }
-                if (e != null) {
-                    e.setAttribute("ui.class", "highlighted");
+                if (e != null) {                  //n*n
+                    e.setAttribute("ui.class", "highlighted");                  //n*n
                 }
             }
         }
 
-        for (Vertice v : subGrafo.obtenerVertices()) {
-            Node n = g.getNode(v.getNombre());
-            n.setAttribute("ui.class", "gris");
+        for (Vertice v : subGrafo.obtenerVertices()) {                  //2n +1
+            Node n = g.getNode(v.getNombre());                  //3n
+            n.setAttribute("ui.class", "gris");                   //n
         }
 
-        return createPanelGrafo(g, false);
+        return createPanelGrafo(g, false);                  //14
     }
 
     /**
@@ -205,9 +205,9 @@ public class PanelGrafo {
      * @param distAcumulada Mapa de cada vértice al peso acumulado desde el
      * origen.
      */
-    public static JPanel obtenerGrafoPintadoRuta(GrafoTDA subGrafoRuta,
+    public static JPanel obtenerGrafoPintadoRuta(GrafoTDA subGrafoRuta, //                          41n"2 + 40n + 24                                                         
             Map<Vertice, Double> distAcumulada) {
-        Graph g = crearGrafoChiapas();
+        Graph g = crearGrafoChiapas();      // 19n"2    +   18n   + 9
 
         g.setAttribute("ui.stylesheet", """
             node {
@@ -227,37 +227,37 @@ public class PanelGrafo {
               fill-color: red;         /* arista en el camino */
               size: 3px;
             }
-        """);
+        """);                  //1
 
-        for (Vertice u : subGrafoRuta.obtenerVertices()) {
-            for (Arista a : subGrafoRuta.obtenerAdyacentes(u)) {
-                String id1 = a.getOrigen().getNombre() + "-" + a.getDestino().getNombre();
-                Edge e = g.getEdge(id1);
-                if (e == null) {
-                    e = g.getEdge(a.getDestino().getNombre() + "-" + a.getOrigen().getNombre());
+        for (Vertice u : subGrafoRuta.obtenerVertices()) {                  //2n +1 
+            for (Arista a : subGrafoRuta.obtenerAdyacentes(u)) {                  //(2n +1)*n
+                String id1 = a.getOrigen().getNombre() + "-" + a.getDestino().getNombre();                  //7n*n
+                Edge e = g.getEdge(id1);                  //2n*n
+                if (e == null) {                  //1n*n
+                    e = g.getEdge(a.getDestino().getNombre() + "-" + a.getOrigen().getNombre());                  //8n*n
                 }
-                if (e != null) {
-                    e.setAttribute("ui.class", "highlighted");
+                if (e != null) {                  //1n*n
+                    e.setAttribute("ui.class", "highlighted");                  //n*n
                 }
             }
         }
 
-        for (Vertice v : subGrafoRuta.obtenerVertices()) {
-            Node n = g.getNode(v.getNombre());
-            if (n != null) {
-                Double d = distAcumulada.get(v);
-                String etiqueta = v.getNombre() + (d != null ? " (" + d.intValue() + ")" : "");
-                n.setAttribute("ui.label", etiqueta);
-                n.setAttribute("ui.class", "path");
+        for (Vertice v : subGrafoRuta.obtenerVertices()) {                            //2n +1
+            Node n = g.getNode(v.getNombre());                  //3n
+            if (n != null) {                                    //n
+                Double d = distAcumulada.get(v);                  //2n
+                String etiqueta = v.getNombre() + (d != null ? " (" + d.intValue() + ")" : "");                  //9n
+                n.setAttribute("ui.label", etiqueta);                   //n
+                n.setAttribute("ui.class", "path");                     //n
             }
         }
 
-        SwingViewer viewer = new SwingViewer(g, SwingViewer.ThreadingModel.GRAPH_IN_GUI_THREAD);
-        viewer.disableAutoLayout();
-        ViewPanel vp = (ViewPanel) viewer.addDefaultView(false);
-        vp.setPreferredSize(new Dimension(800, 600));
-        vp.setMinimumSize(new Dimension(400, 300));
-        return vp;
+        SwingViewer viewer = new SwingViewer(g, SwingViewer.ThreadingModel.GRAPH_IN_GUI_THREAD);                      //3
+        viewer.disableAutoLayout();                                                                                   //1
+        ViewPanel vp = (ViewPanel) viewer.addDefaultView(false);                  //3
+        vp.setPreferredSize(new Dimension(800, 600));                  //2
+        vp.setMinimumSize(new Dimension(400, 300));                  //2
+        return vp;                  //1
     }
 
     
@@ -268,11 +268,11 @@ public class PanelGrafo {
      * @param colorMap        Mapa de cada vértice a su ColorVertice (BLANCO/GRIS/NEGRO)
      * @param subGrafoRecorrido GrafoTDA con las aristas del árbol de recorrido
      */
-    public static JPanel obtenerGrafoPintadoRecorrido(
+    public static JPanel obtenerGrafoPintadoRecorrido(                                                               //                                    40n"2 + 31n + 23
             Map<Vertice, ColorVertice> colorMap,
             GrafoTDA subGrafoRecorrido) {
 
-        Graph g = crearGrafoChiapas();
+        Graph g = crearGrafoChiapas();      // 19n"2    +   18n   + 9
 
         g.setAttribute("ui.stylesheet", """
             node {
@@ -286,38 +286,38 @@ public class PanelGrafo {
             node.negro  { fill-color: gray; }
             edge { fill-color: rgb(130,130,130); size: 2px; }
             edge.highlighted { fill-color: red; size: 3px; }
-        """);
+        """);                  //1
 
-        for (Vertice u : subGrafoRecorrido.obtenerVertices()) {
-            for (Arista a : subGrafoRecorrido.obtenerAdyacentes(u)) {
-                String id1 = a.getOrigen().getNombre() + "-" + a.getDestino().getNombre();
-                Edge e = g.getEdge(id1);
-                if (e == null) {
-                    e = g.getEdge(a.getDestino().getNombre() + "-" + a.getOrigen().getNombre());
+        for (Vertice u : subGrafoRecorrido.obtenerVertices()) {                  //2n+1
+            for (Arista a : subGrafoRecorrido.obtenerAdyacentes(u)) {                  //(2n+1)*n
+                String id1 = a.getOrigen().getNombre() + "-" + a.getDestino().getNombre();                  //7n*n
+                Edge e = g.getEdge(id1);                         //2n*n
+                if (e == null) {                  //n*n
+                    e = g.getEdge(a.getDestino().getNombre() + "-" + a.getOrigen().getNombre());                  //7n*n
                 }
-                if (e != null) {
-                    e.setAttribute("ui.class", "highlighted");
+                if (e != null) {                  //n*n
+                    e.setAttribute("ui.class", "highlighted");                  //n*n
                 }
             }
         }
 
-        for (Map.Entry<Vertice, ColorVertice> entry : colorMap.entrySet()) {
-            Vertice v = entry.getKey();
-            ColorVertice cv = entry.getValue();
-            Node n = g.getNode(v.getNombre());
-            if (n != null) {
+        for (Map.Entry<Vertice, ColorVertice> entry : colorMap.entrySet()) {                      //2n+1
+            Vertice v = entry.getKey();                  //2n
+            ColorVertice cv = entry.getValue();                  //2n
+            Node n = g.getNode(v.getNombre());                  //3n
+            if (n != null) {                  //n
                 // asignamos blanco/gris/negro
-                n.setAttribute("ui.class", cv.name().toLowerCase());
+                n.setAttribute("ui.class", cv.name().toLowerCase());                  //3n
             }
         }
 
         SwingViewer viewer = new SwingViewer(g,
-            SwingViewer.ThreadingModel.GRAPH_IN_GUI_THREAD);
-        viewer.disableAutoLayout();
-        ViewPanel vp = (ViewPanel) viewer.addDefaultView(false);
-        vp.setPreferredSize(new Dimension(800, 600));
-        vp.setMinimumSize(new Dimension(400, 300));
-        return vp;
+            SwingViewer.ThreadingModel.GRAPH_IN_GUI_THREAD);                  //2
+        viewer.disableAutoLayout();                  //1
+        ViewPanel vp = (ViewPanel) viewer.addDefaultView(false);                  //3
+        vp.setPreferredSize(new Dimension(800, 600));                  //2
+        vp.setMinimumSize(new Dimension(400, 300));                  //2
+        return vp;                  //1
     }
     
     
